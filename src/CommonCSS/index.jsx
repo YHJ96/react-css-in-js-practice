e5c109895dfeed2c722779f7a19f46edbaab6a10
YHJ96 <yhj878812@naver.com>
2022-09-13 00:15:45 +0900

    feat: 리액트 CSS 기본 사용법 추가 완료

--- src/CommonCSS/index.jsx
+++ src/CommonCSS/index.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import './styles.css';
+
+function PropsButton(props) {
+  const styles = {
+    border: `2px solid ${props.color}`,
+    backgroundColor: props.color,
+    color: 'white',
+    padding: '16px',
+    textAlign: 'center',
+    fontSize: '16px',
+    cursor: 'pointer',
+  };
+  return <button style={styles}>{props.children}</button>;
+}
+
+PropsButton.defaultProps = {
+  color: 'blue',
+};
+
+function ExtendButton(props) {
+  const extendStlye = {
+    ...props.styles,
+    fontSize: '20px',
+  };
+  return <button style={extendStlye}>{props.children}</button>;
+}
 
 function CommonCSS() {
-  return <div>CommonCSS</div>;
+  const styles = {
+    border: `2px solid blue`,
+    backgroundColor: 'blue',
+    color: 'white',
+    padding: '16px',
+    textAlign: 'center',
+    fontSize: '16px',
+    cursor: 'pointer',
+  };
+  return (
+    <div>
+      <button className="basic-button">기본 버튼</button>
+      <PropsButton color="red">Props 버튼</PropsButton>
+      <PropsButton color="purple">Props 버튼</PropsButton>
+      <ExtendButton styles={styles}>Extend 버튼</ExtendButton>
+    </div>
+  );
 }
 
 export default CommonCSS;
