4d41f437b06dc55b04cf81edbefd36caa2fb9bbe
YHJ96 <yhj878812@naver.com>
2022-09-12 20:45:11 +0900

    feat: Props 컴포넌트 사용법 확인 완료

--- src/StyledComponent/index.jsx
+++ src/StyledComponent/index.jsx
@@ -12,10 +12,29 @@ const BasicButton = styled.button`
   cursor: pointer;
 `;
 
+// 스타일 컴포넌트는 컴포넌트의 프로퍼티를 받아서 사용할 수 있다.
+const PropsButton = styled.button`
+  border: 2px solid ${(props) => props.color};
+  background-color: ${(props) => props.color};
+  color: white;
+  padding: 16px;
+  text-align: center;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+// defaultProps도 설정 가능하다.
+PropsButton.defaultProps = {
+  color: 'blue',
+};
+
 function StyledComponent() {
   return (
     <div>
       <BasicButton>기본 버튼</BasicButton>
+      <PropsButton color="red">Props 버튼</PropsButton>
+      <PropsButton color="purple">Props 버튼</PropsButton>
+      <PropsButton>Props 버튼</PropsButton>
     </div>
   );
 }
