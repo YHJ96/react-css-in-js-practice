d8c15d638d5f84bc54fdcf00bad6dc45f65cdaf4
YHJ96 <yhj878812@naver.com>
2022-09-12 20:36:30 +0900

    feat: 기본 사용법 확인 완료

--- src/StyledComponent/index.jsx
+++ src/StyledComponent/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// 스타일 컴포넌트 기본 사용법
+const BasicButton = styled.button`
+  border: 2px solid green;
+  background-color: green;
+  color: white;
+  padding: 16px;
+  text-align: center;
+  font-size: 16px;
+  cursor: pointer;
+`;
 
 function StyledComponent() {
-  return <div>StyledComponent</div>;
+  return (
+    <div>
+      <BasicButton>기본 버튼</BasicButton>
+    </div>
+  );
 }
 
 export default StyledComponent;
